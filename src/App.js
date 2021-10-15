b2f6b11ea2d1c756d7f397ece641f633179e5638
vishalsanguri <vishal30dev@gmail.com>
2021-10-15 22:50:03 +0530

    start

--- src/App.js
+++ src/App.js
@@ -1,18 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Register from "./components/Register";
-import Login from "./components/Login";
+
 export default function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Register />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-      </Switch>
-    </Router>
-  );
+  return <div>hi</div>;
 }
